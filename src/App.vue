6af5decd93f2856eca66718c279c2997540e1c40
Markov Roman <56278713+markovrv@users.noreply.github.com>
2023-12-14 06:50:45 +0000

    2 лекция

--- src/App.vue
+++ src/App.vue
@@ -23,38 +23,59 @@
 
     <div class="container">
       <b-row>
-        <b-col cols="2">меню</b-col>
+        <b-col cols="2">
+
+        <div class="mt-2">Поиск</div>
+        <b-form-input v-model="text"></b-form-input>
+        <br>
+        <div class="mt-2">Категории</div>
+        <b-list-group>
+          <b-list-group-item
+            @click="selectCategory(category)"
+            v-for="(category,id) in categories"
+            :key="id"
+            :active="category==currentCat"
+          >{{category}}</b-list-group-item>
+          <b-list-group-item
+            @click="selectCategory('')"
+            :active="currentCat==''"
+          >Все категории</b-list-group-item>
+        </b-list-group>
+        
+        </b-col>
         <b-col cols="10">
           <b-jumbotron header="BootstrapVue" lead="Компоненты Bootstrap v4 для Vue.js 2">
             <p>Для получения дополнительной информации посетите веб-сайт</p>
             <b-button variant="primary" href="#">Больше информации</b-button>
           </b-jumbotron>
           <hr>
+          <b-row>
+            <b-col cols="3" v-for="(good,id) in searchInGoods()" :key="id">
 
-
-        <b-card
-          title="Заголовок карточки"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Изображение"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mb-2"
-        >
-          <b-card-text>
-            Небольшой пример текста, который будет построен на заголовке карты и составляет основную часть содержимого карты.
-          </b-card-text>
-
-          <b-button href="#" variant="primary">Иди куда-нибудь</b-button>
-        </b-card>
+              <b-card
+                
+                :title="good.title"
+                :img-src="good.image"
+                
+                img-alt="Изображение"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+              >
+                <b-card-text>
+                  {{ good.description }}
+                </b-card-text>
+                <h3>{{good.price}}</h3>
+                <b-button href="#" variant="primary">Купить</b-button> 
+              </b-card>
+            </b-col>
+          </b-row>
 
 
         </b-col>
       </b-row>
     
-
-
-
     </div>
   </div>
 </template>
@@ -62,6 +83,45 @@
 <script>
 export default {
   name: 'App',
+  data: () => ({
+    goods: [],
+    categories: [],
+    currentCat: '',
+    text: ''
+  }),
+  methods: {
+    getAllGoods() {
+      fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{
+              this.goods = json
+            })
+    },
+    getAllCategories() {
+      fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=>{
+              this.categories = json
+            })
+    },
+    searchInGoods() {
+      var goods = []
+      if(this.currentCat == '') goods = this.goods
+      else goods = this.goods.filter(item => (item.category == this.currentCat))
+
+      if (this.text == '') return goods
+      return goods.filter((item) => {
+        return item.title.indexOf(this.text) >= 0
+      })
+    },
+    selectCategory(item){
+      this.currentCat = item
+    }
+  },
+  created() {
+    this.getAllGoods()
+    this.getAllCategories()
+  }
 }
 </script>
 
