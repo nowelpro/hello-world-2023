bc98348e6c402512fbd7d22ad7fdc4f53975efed
nowelpro <97058752+nowelpro@users.noreply.github.com>
2023-12-25 20:19:29 +0000

    magasin

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,289 @@
 <template>
   <div id="app">
+    <header>
 
-  <b-navbar toggleable="sm" type="light" variant="light">
-
-    <div class="container">
-
-        <b-navbar-brand href="#">Магазин</b-navbar-brand>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#">Ссылка</b-nav-item>
-            <b-nav-item href="#" disabled>Отключено</b-nav-item>
-          </b-navbar-nav>
-
-        </b-collapse>
-
-    </div>
-
-  </b-navbar>
-
+      <div id="navbarHeader" class="bg-dark collapse" style="">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">О нас</h4>
+              <p class="text-muted">Добавьте информацию об альбоме ниже, авторе или любом другом фоновом
+                контексте. Составьте несколько предложений, чтобы люди могли почерпнуть некоторые полезные
+                сведения. Затем свяжите их с некоторыми сайтами социальных сетей или с контактной
+                информацией.</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Связаться с нами</h4>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white">Следуйте в Twitter</a></li>
+                <li><a href="#" class="text-white">Лайк в Facebook</a></li>
+                <li><a href="#" class="text-white">Email</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container"><a href="#" class="navbar-brand d-flex align-items-center"><svg
+              xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+              stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true"
+              Посмотретьbox="0 0 24 24" class="mr-2">
+              <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
+              <circle cx="12" cy="13" r="4"></circle>
+            </svg> <strong>Superstarый.fakeshop</strong></a>
+          <form class="d-flex"><input type="search" placeholder="Найти товар/продавца" aria-label="Search" class="form-control mr-2">
+          </form> <button type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader"
+            aria-expanded="false" a ria-label="Toggle navigation" class="navbar-toggler collapsed"><span
+              class="navbar-toggler-icon"></span></button>
+        </div>
+      </div>
+    </header>
     <div class="container">
       <b-row>
-        <b-col cols="2">меню</b-col>
         <b-col cols="10">
-          <b-jumbotron header="BootstrapVue" lead="Компоненты Bootstrap v4 для Vue.js 2">
-            <p>Для получения дополнительной информации посетите веб-сайт</p>
-            <b-button variant="primary" href="#">Больше информации</b-button>
+          <b-jumbotron header="Ulito"
+            lead="Лучший маркетплейс для покупки новых и БУ товаров, с доставкой на дом самокатом">
+            <p>Для получения дополнительной информации посетите веб-сайт: <b-button variant="primary" href="#">ulito-dostavka</b-button></p>
           </b-jumbotron>
-          <hr>
-
-
-        <b-card
-          title="Заголовок карточки"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Изображение"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mb-2"
-        >
-          <b-card-text>
-            Небольшой пример текста, который будет построен на заголовке карты и составляет основную часть содержимого карты.
-          </b-card-text>
-
-          <b-button href="#" variant="primary">Иди куда-нибудь</b-button>
-        </b-card>
 
 
         </b-col>
       </b-row>
-    
 
+      
+      <div class="container py-5 bg-dark">
+       
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;">
+                <img
+                  src="https://hors.by/upload/resize_cache/webp/iblock/092/as92jbvz968f5xlbfo2foaxvfs6iwl73/mototsikl_racer_rc200gy_c2_enduro_023_min.webp"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Racer RC200GY-C2 Enduro</h5>
+                  <h3 class="card-title">146 900 ?</h3>
+                  <p class="card-text">Новинка 2017 Мотоцикл Racer RC200GY-C2 Enduro - это знакомый многим представитель класса внедорожников . Обновленная модель получила значительные изменения в дизайне: установлена защита рук, двигателя и багажник, увеличен объем топливного бака до 5 литров, установлен хорошо знакомый и проверенный двигатель 164 FML 200 сс. Ну и пожалуй самое значительное новшество, это то, что теперь данная модель будет поставляться с ПТС, что позволяет эксплуатацию данного мотоцикла на дорогах общего пользования... Итогом всех изменений по ENDURO 200 явилось снижение его розничной стоимости на 20%, что делает эту модель еще более привлекательной!</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Заказать
 
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;">
+                <img src="https://vezdehod38.ru/upload/medialibrary/0f9/0f940c5a8015e65f4199ad12b9e2c738.jpg"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Квадроцикл gladiator F200 LUX</h5>
+                  <h3 class="card-title">209 000 ?</h3>
+                  <p class="card-text">НОВИНКА 2021 года!
+
+Квадроцикл порадует широким, массивным кузовом и отличным дополнительным оборудованием.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">
+                        Заказать</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;">
+                <img src="https://avatars.mds.yandex.net/get-autoru-vos/2189734/4caba1e1e139d121c5bc652cd9c374fc/1200x900n"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Квадроцикл stels ATV 650 G Guepard CVTech</h5>
+                  <h3 class="card-title">799 900 ?</h3>
+                  <p class="card-text">Широкая комплектация, стильный окрас и непревзойденная мощь квадроцикла STELS ATV 650 GUEPARD TROPHY позволяют вытеснить конкурентов с первых строчек рейтинга. Любые горки, бездорожье, да и гоночные трассы с легкостью поддадутся вам и доставят необычайные впечатления.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://main-cdn.sbermegamarket.ru/big2/hlr-system/185/194/459/042/821/50/100052414627b0.jpg" class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">WHITE SIBERIA PRO BLACK EDITION 5000W</h5>
+                  <h3 class="card-title">215 000 ?</h3>
+                  <p class="card-text">Настраиваемая подвеска, можно настроить под себя;
+•    Большие гидравлические тормоза;
+•    Съёмный аккумулятор Li-Ion 60V 31Ah с балансировочной платой;
+•    Мотор-колесо мощностью 5000W;
+•    Совершенно новый, дерзкий внешний вид;
+•    Большое, удобное, мягкое сидение;
+•    Отделение для небольших предметов и документов под сидением;
+•    Площадка для крепления бокса или корзинки;
+•    Яркая LED фара с линзой и указатели поворотов;
+•    Большой дисплей бортового компьютера;
+•    Максимальная скорость до 85 км/ч.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://morekurtok.ru/upload/iblock/c34/tapochki_s_vyshivkoy_ya_zdes_glavnyy.jpg"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">ТАПКИ Я ЗДЕСЬ
+                  </h5>
+                  <h3 class="card-title">800 ?</h3>
+                  <p class="card-text">Домашние тапочки мужские и женские от бренда Emotion Day – прекрасный вариант обуви для дома и дачи. Модель "Пивозавр" выполнена из велюра. Подошва изготовлена из материала ЭВА, который славится легкостью, эластичностью и теплозащитными свойствами. Яркий, оригинальный дизайн будет поднимать Вам настроение по утрам, а мягкость и уют помогать расслабиться вечером после трудного рабочего дня. </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://cs10.pikabu.ru/post_img/2018/09/05/7/og_og_153614208527376453.jpg"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">ТАПКИ РАКЕТА</h5>
+                  <h3 class="card-title">4 599 ?</h3>
+                  <p class="card-text">Натурелла Хоум создаёт ЭКО стиль (состав тапочек: подошва натуральный войлок 100%, мех: 30% натуральная овечья шерсть, 70% полиэстер), которому присущи простота, сочетание практичности и демократичности функционального дизайна, который мы разработали на основе сочетания натуральных и повторно переработанных материалов. Плюшевые теплые закрытые тапки из овчины на войлочной подошве обладают прекрасной терморегуляцией и расслабляющим массажным эффектом, не раздражают кожу, гипоаллергенны</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://cache3.youla.io/files/images/780_780/5a/bd/5abdd724c15ae343e674c262.jpg"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">ЛИНОЛЕУМ БУ</h5>
+                  <h3 class="card-title">666 ?</h3>
+                  <p class="card-text">ЛИНОЛИУМ ИЗ ПОД ДЕДА, ИСПОЛИЗОВАЛИСЬ НЕДОЛГО
+Комплектация;
+Размер куска 3 на 1
+Важно! Если заказываете например 2шт. Вам привозят два куска по 3 на 1 раздельно, НЕ одним куском целым. Заказываете 3шт. привозят 3шт. размером 3 на 1 и т. д.
+Линолеум полукоммерческий, очень хорошего качества. Толстый на войлочной теплой основе 3.8мм. Линолеум полукоммерческий 32 класс.
+Сфера применения: полукоммерческий
+Класс помещения: 32
+Толщина: 3.8 мм
+Толщина защитного слоя: 0.50 мм
+Вес: 2.5 кг/м2.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://img2.festima.ru/1/7AbTDAiWeTRw26wWE"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">СРЕМЯНКА СОВЕСТКАЯ</h5>
+                  <h3 class="card-title">1 000 ?</h3>
+                  <p class="card-text">Cтальная стрeмянка 4 ступени СССP
+Онa сoвpеменнaя, не СCCP
+У нac oна простo дaвнo.
+
+Выcoтa до площадки 100 см (1 метр )
+Bcё время лежaлa на oткpытoй лoджии ( попадали оcадки).
+Для меня она шаткая и хилaя ,но женa вeшала шторы с нeё .
+
+Для домa и нe тяжёлoго чeлoвека вполнe нopмальный помощник.
+                  </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://i.pinimg.com/736x/85/a7/ba/85a7bae327cf7cbfc6d9b5562d8cddfc.jpg"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">ТАПКИ ИЗ ГЕРМАНИИ</h5>
+                  <h3 class="card-title">1 999 ?</h3>
+                  <p class="card-text">Плюшевые мужские тапочки это незаменимый домашний атрибут в осенне-весенний и зимний сезон. Стельки выполнена из меховой ткани - тактильно-приятная, снимает напряжение после работы, а закрытый мыски согревают ногу. Прорезиновые вставки предотвращает скольжении. Домашние тапки будут отличным подарком на день рождения, 23 февраля и другие праздники, а дополнив их махровыми костюмами ТВОЕ, вы получите отличный лук на пижамные вечеринки или для тематической фотосессии.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://cstor.nn2.ru/forum/data/forum/images/2018-10/215054252-1007476904.jpg" class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">КРУЖКА АНГРИ БЕРД</h5>
+                  <h3 class="card-title">734 ?</h3>
+                  <p class="card-text">Керамическая кружка-сувенир для чая или кофе с глянцевым покрытием и оригинальным принтом станет Мега крутым подарком на любой праздник для Ваших близких, родных, коллег. Наша кружка не просто интересный подарок, но и необходимая вещь в быту. Пейте чай каждый день, поднимайте себе настроение и заряжайтесь позитивом!
+
+Кружка 100% состоит из керамики. Яркий насыщенный принт качественно нанесен на поверхность кружки. Рекомендуем мыть кружечку мягкими губками без абразива, вручную. Средство посудомоечных машин может повредить изображение. МЫТЬ КРУЖКУ В ПОСУДОМОЕЧНОЙ МАШИНЕ НЕЛЬЗЯ! Можно использовать в СВЧ-печи.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://ir.ozone.ru/s3/multimedia-z/wc1000/6335205827.jpg"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">КРУЖКА НА ПАПЕЙ </h5>
+                  <h3 class="card-title">412 ?</h3>
+                  <p class="card-text">Кружка 100% состоит из керамики. Яркий насыщенный принт качественно нанесен на поверхность кружки. Рекомендуем мыть кружечку мягкими губками без абразива, вручную. Средство посудомоечных машин может повредить изображение. МЫТЬ КРУЖКУ В ПОСУДОМОЕЧНОЙ МАШИНЕ НЕЛЬЗЯ! Можно использовать в СВЧ-печи.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm" style="padding: 12px;"><img
+                  src="https://thumbs.dreamstime.com/b/%D0%B7%D1%83%D0%B1%D0%BD%D0%B0%D1%8F-%D1%89%D0%B5%D1%82%D0%BA%D0%B0-%D0%BF%D1%80%D0%B5-%D0%BF%D0%BE%D1%81%D1%8B-%D0%BA%D0%B8-%D1%87%D0%B5%D1%80%D0%BD%D0%B0%D1%8F-88111995.jpg"
+                  class="bd-placeholder-img card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">ЩЕТКА НОВАЯ</h5>
+                  <h3 class="card-title">1 001 ?</h3>
+                  <p class="card-text">Мануальная зубная щетка Oral-B Complex «Глубокая чистка» бережно очищает всю полость рта, включая задние зубы и труднодоступные области, обеспечивая эффективное удаление зубного налета. Массажные щетинки мягко массируют и стимулируют десны и помогают очищать зубы вдоль линии десен. Щетинки Power Tip проникают в труднодоступные места между зубами и бережно очищают эмаль. Щетка оснащена подушечкой для чистки языка. Зубная щетка с щетинками средней жесткости. Не забывайте менять свою щетку раз в три месяца. Oral-B – №1 марка, рекомендуемая большинством стоматологов в мире на основании результатов опросов, проведенных с ноября 2019 г. по июнь 2021 г.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><button type="button"
+                        class="btn btn-sm btn-outline-secondary">Заказать</button></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
-  </div>
+ 
 </template>
 
 <script>
-export default {
-  name: 'App',
-}
+  export default {
+    name: 'App',
+  }
 </script>
 
-<style>
-</style>
+<style></style>
